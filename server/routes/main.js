de988a13928db7b6e833061951f54ec0ec4f1800
themilnera <amilner32@gmail.com>
2024-11-25 13:37:49 -0700

    fixed problems

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,13 +1,19 @@
-// const express = require("express");
-// const router = express.Router();
-// const Post = require("../models/Post");
+const express = require("express");
+const router = express.Router();
+const Post = require("../models/Post");
 
-// router("/", async (req, res) =>{
-//     const locals = (
-//         title: "NodeJS Blog",
-//         description:"A blog template application",
-//     );
-//     try {
-//         const data = await Post.find().sort({ c})
-//     }
-// });
+router.get("/", async (req, res) =>{
+    const locals = {
+        title: "NodeJS Blog",
+        description:"A blog template application",
+    };
+    try {
+        const data = await Post.find().sort({ createdAt: "desc" });
+        res.render("index", { locals, data });
+    }
+    catch(error){
+        console.log(error);
+    }
+});
+
+module.exports = router;
