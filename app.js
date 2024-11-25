d6abe3963e727baabc1c157d49e64aa6984a63d2
themilnera <amilner32@gmail.com>
2024-11-25 12:44:56 -0700

    other stuff

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 require("dotenv").config();
+
+const expressLayouts = require("express-ejs-layouts");
 const express = require("express");
 const app = express();
 const PORT = 4000;
@@ -6,10 +8,21 @@ const PORT = 4000;
 const connectDB = require("./server/config/db");
 connectDB();
 
-app.get("/", (req, res) =>{
-    res.send("Hello World and so on and all that");
-})
+//app.use sets up middleware functions 
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 
+app.use(expressLayouts);
+app.use(express.static("public"));
+app.set("layout", "./layouts/main");
+app.set("view engine", "ejs");
+
+app.use("/", require("./server/routes/main"))
+//middleware functions run for all HTTP requests (GET, POST, etc)
+//they run in the order they are defined
+//app.use([path,] middleware)
+
+//not middleware
 app.listen(PORT, ()=>{
     console.log(`server is running on port: ${PORT}`);
-})
+});
