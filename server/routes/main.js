1c60df04b49cfe2f7bc3f943b6a5f186a806ec86
themilnera <amilner32@gmail.com>
2024-11-25 13:01:26 -0700

    added stuff, it's broken

---
+++ server/routes/main.js
@@ -0,0 +1,13 @@
+// const express = require("express");
+// const router = express.Router();
+// const Post = require("../models/Post");
+
+// router("/", async (req, res) =>{
+//     const locals = (
+//         title: "NodeJS Blog",
+//         description:"A blog template application",
+//     );
+//     try {
+//         const data = await Post.find().sort({ c})
+//     }
+// });
