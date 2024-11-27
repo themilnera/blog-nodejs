0604007e913c4d733bb8dfe5ad25c9b8f3585655
themilnera <amilner32@gmail.com>
2024-11-27 13:23:52 -0700

    finishing project

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -4,111 +4,213 @@ const Post = require("../models/Post");
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const jwtSecret= process.env.JWT_SECRET;
+const jwtSecret = process.env.JWT_SECRET;
 const adminLayout = "../views/layouts/admin";
 
-const authMiddleware = (req, res, next) =>{
-    const token = req.cookies.token;
+/**
+ * Check Login Middleware
+ */
+const authMiddleware = (req, res, next) => {
+  const token = req.cookies.token;
 
-    if(!token){
-        return res.status(401).json({ message: "Unauthorized" });
-    }
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
 
-    try{
-        const decoded = jwt.verify(token, jwtSecret);
-        req.userId = decoded.userId;
-        next();
-    }
-    catch(error){
-        return res.status(401).json({ message: "Unauthorized" });
-    }
-}
+  try {
+    const decoded = jwt.verify(token, jwtSecret);
+    req.userId = decoded.userId;
+    next();
+  } catch (error) {
+    res.status(401).json({ message: "Unauthorized" });
+  }
+};
 
-
-
+/**
+ * GET /
+ * Admin - Check Login
+ */
 router.get("/admin", async (req, res) => {
-    try{
-        const locals = {
-            title: "Admin",
-            description: "A blog template made with NodeJS and ExpressJS",
-        };
-        res.render("admin/index", { locals, layout: adminLayout });
-    }
-    catch(error){
-        console.log(error);
-    }
+  try {
+    const locals = {
+      title: "Admin",
+      description: "A blog template made with NodeJS and ExpressJS",
+    };
+
+    res.render("admin/index", { locals, layout: adminLayout });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
+/**
+ * POST /
+ * Admin - Check Login
+ */
 router.post("/admin", async (req, res) => {
-    try{
-        const { username, password } = req.body;
-        const user = await User.findOne({ username });
+  try {
+    const { username, password } = req.body;
 
-        if(!user){
-            return res.status(401).json({ message: "Invalid credentials" });
-        }
-        const isPasswordValid = await bcrypt.compare(password, user.password);
+    const user = await User.findOne({ username });
 
-        if(!isPasswordValid){
-            return res.status(401).json({ message: "Invalid Credentials "});
-        }
-
-        const token = jwt.sign({ userId: user._id }, jwtSecret);
-        res.cookie("token", token, { httpOnly: true });
-        res.redirect("/dashboard");
+    if (!user) {
+      return res.status(401).json({ message: "Invalid credentials" });
     }
-    catch(error){
-        console.log(error);
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: "Invalid credentials" });
     }
+
+    const token = jwt.sign({ userId: user._id }, jwtSecret);
+    res.cookie("token", token, { httpOnly: true });
+    res.redirect("/dashboard");
+  } catch (error) {
+    console.log(error);
+  }
 });
 
+/**
+ * GET /
+ * Admin - Dashboard
+ */
 router.get("/dashboard", authMiddleware, async (req, res) => {
-    try{
-        const locals = {
-            title: "Dashboard",
-            description: "A blog template made with NodeJS and ExpressJS",
-        };
+  try {
+    const locals = {
+      title: "Dashboard",
+      description: "A blog template made with NodeJS and ExpressJS, and EJS",
+    };
 
-        const data = await Post.find();
-        res.render("admin/dashboard", { locals, data, layout: adminLayout });
-    }
-    catch(error) {}
+    const data = await Post.find();
+    res.render("admin/dashboard", { locals, data, layout: adminLayout });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
-router.post("/register", async(req, res) => {
-    try{
-        const { username, password } = req.body;
-        const hashedPassword = await bcrypt.hash(password, 10);
+/**
+ * GET /
+ * Admin - Create New Post
+ */
+router.get("/add-post", authMiddleware, async (req, res) => {
+  try {
+    const locals = {
+      title: "Create Post",
+      description: "A blog template made with NodeJS and ExpressJS, and EJS",
+    };
 
-        try{
-            const user = await User.create({
-                username,
-                password: hashedPassword,
-            });
-            res.status(201).json({ message: "User created successfully", user});
-        }
-        catch(error){
-            if(error === 11000){
-                return res.status(500).json({ message: "User already exists!" });
-            }
-            else{
-                return res.status(500).json({ message: "Something went wrong with the server!" });
-            }
-        }
-    }
-    catch(error){
-        console.log(error);
-    }
+    const data = await Post.find();
+    res.render("admin/add-post", { locals, data, layout: adminLayout });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
-router.get("/logout", authMiddleware, async(req, res) => {
-    try{
-        res.clearCookie("token");
-        res.redirect("/");
+/**
+ * POST /
+ * Admin - Create New Post
+ */
+router.post("/add-post", authMiddleware, async (req, res) => {
+  try {
+    console.log(req.body);
+    try {
+      const newPost = new Post({
+        title: req.body.title,
+        body: req.body.body,
+      });
+      await Post.create(newPost);
+      res.redirect("/dashboard");
+    } catch (error) {
+      console.log(error);
     }
-    catch(error){
-        console.log(error);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+/**
+ * GET /
+ * Admin - Update Post
+ */
+router.get("/edit-post/:id", authMiddleware, async (req, res) => {
+  try {
+    const locals = {
+      title: "Edit Post",
+      description: "A blog template made with NodeJS and ExpressJS, and EJS",
+    };
+
+    const data = await Post.findOne({ _id: req.params.id });
+    res.render("admin/edit-post", { locals, data, layout: adminLayout });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+/**
+ * PUT /
+ * Admin - Edit Post
+ */
+router.put("/edit-post/:id", authMiddleware, async (req, res) => {
+  try {
+    await Post.findByIdAndUpdate(req.params.id, {
+      title: req.body.title,
+      body: req.body.body,
+      updatedAt: Date.now(),
+    });
+    res.redirect(`/edit-post/${req.params.id}`);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+/**
+ * DELETE /
+ * Admin - Delete Post
+ */
+router.delete("/delete-post/:id", authMiddleware, async (req, res) => {
+  try {
+    await Post.deleteOne({ _id: req.params.id });
+    res.redirect("/dashboard");
+  } catch (error) {
+    console.log(error);
+  }
+});
+/**
+ * GET /
+ * Admin - Logout
+ */
+router.get("/logout", authMiddleware, async (req, res) => {
+  try {
+    res.clearCookie("token");
+    res.redirect("/");
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// POST - Admin - Register
+router.post("/register", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    try {
+      const user = await User.create({
+        username,
+        password: hashedPassword,
+      });
+      res.status(201).json({ message: "User created successfully", user });
+    } catch (error) {
+      if (error === 11000) {
+        res.status(500).json({ message: "Username already exists" });
+      } else {
+        res.status(500).json({ message: "Something went wrong" });
+      }
     }
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 module.exports = router;
--- server/routes/main.js
+++ server/routes/main.js
@@ -2,83 +2,94 @@ const express = require("express");
 const router = express.Router();
 const Post = require("../models/Post");
 
+// Home Route with Pagination
 router.get("/", async (req, res) => {
-    try {
-      const locals = {
-        title: "NodeJs Blog",
-        description: "Simple Blog created with NodeJs, Express & MongoDb.",
-      };
-  
-      let perPage = 3;
-      let page = req.query.page || 1;
-  
-      const data = await Post.aggregate([{ $sort: { title: -1 } }])
-        .skip(perPage * page - perPage)
-        .limit(perPage)
-        .exec();
-  
-      // Count is deprecated - please use countDocuments({}) instead
-      // const count = await Post.count();
-      const count = await Post.countDocuments({});
-      const nextPage = parseInt(page) + 1;
-      const hasNextPage = nextPage <= Math.ceil(count / perPage);
-      const hasNextPagePlus = nextPage <= Math.ceil(count * perPage);
-  
-      res.render("index", {
-        locals,
-        data,
-        current: page,
-        nextPage: hasNextPage ? nextPage : null,
-        prevPage: hasNextPagePlus ? page - 1 : null,
-      });
-    } catch (error) {
-      console.log(error);
-    }
-  });
+  try {
+    const locals = {
+      title: "NodeJs Blog",
+      description: "Simple Blog created with NodeJs, Express & MongoDb.",
+    };
 
-//Get Post by Id
+    let perPage = 3;
+    let page = req.query.page || 1;
 
-router.get("/post/:id", async (req, res) =>{
-    try{
-        let slug = req.params.id;
-        const data = await Post.findById({_id: slug});
-        const locals = {
-            title: data.title,
-            description:"A blog template application",
-        };
-        res.render("post", {locals, data});
-    }
-    catch(error){
-        console.log(error);
-    }
+    const data = await Post.aggregate([{ $sort: { title: -1 } }])
+      .skip(perPage * page - perPage)
+      .limit(perPage)
+      .exec();
+
+    // Count is deprecated - please use countDocuments({}) instead
+    // const count = await Post.count();
+    const count = await Post.countDocuments({});
+    const nextPage = parseInt(page) + 1;
+    const hasNextPage = nextPage <= Math.ceil(count / perPage);
+    const hasNextPagePlus = nextPage <= Math.ceil(count * perPage);
+
+    res.render("index", {
+      locals,
+      data,
+      current: page,
+      nextPage: hasNextPage ? nextPage : null,
+      prevPage: hasNextPagePlus ? page - 1 : null,
+      currentRoute: "/",
+    });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
-//Search route
+// Get Posts by Id
+router.get("/post/:id", async (req, res) => {
+  try {
+    let slug = req.params.id;
 
+    const data = await Post.findById({ _id: slug });
+
+    const locals = {
+      title: data.title,
+      description: "A blog template made with NodeJS and ExpressJS",
+    };
+    res.render("post", { locals, data, currentRoute: `/post/${slug}` });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// Search Posts by Id
 router.post("/search", async (req, res) => {
-    try{
-        const locals = {
-            title: "Search",
-            description: "A blog template made with NodeJS and ExpressJS",
-        };
+  try {
+    const locals = {
+      title: "Search",
+      description: "A blog template made with NodeJS and ExpressJS",
+    };
 
-        let searchTerm = req.body.searchTerm;
-        const searchNoSpecialChar = searchTerm.replace(/[^a-zA-Z ]/g, "");
+    let searchTerm = req.body.searchTerm;
+    const searchNoSpecialChar = searchTerm.replace(/[^a-zA-Z ]/g, "");
+    const data = await Post.find({
+      $or: [
+        { title: { $regex: new RegExp(searchNoSpecialChar, "i") } },
+        { body: { $regex: new RegExp(searchNoSpecialChar, "i") } },
+      ],
+    });
 
-        const data = await Post.find({
-            $or: [
-                { title: { $regex: new RegExp(searchNoSpecialChar, "i")}},
-                { body: { $regex: new RegExp(searchNoSpecialChar, "i")}},
-            ],
-        });
+    res.render("search", { locals, data, currentRoute: "/search" });
+  } catch (error) {
+    console.log(error);
+  }
+});
 
-        res.render("search", { locals, data });
-    }
-    catch(error){
-        console.log(error);
-    }
-
-
+// About Route
+// router.get("/about", (req, res) => res.render("admin"));
+router.get("/about", async (req, res) => {
+  try {
+    const locals = {
+      title: "About",
+      description: "A blog template made with NodeJS and ExpressJS",
+    };
+    res.render("about", { locals, currentRoute: "/about" });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 module.exports = router;
